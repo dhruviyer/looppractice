e5386b6f1010e25e59547238dd7110053309a7ec
diyer <diyer@tgen.org>
2015-09-24 08:51:47 -0700

    added method 17

--- Loop Practice/src/LoopPractice.java
+++ Loop Practice/src/LoopPractice.java
@@ -3,7 +3,8 @@ import javax.swing.JOptionPane;
 public class LoopPractice {
 	
 	public static void main(String[] args){
-		method16();
+		//method16();
+		method17();
 	}
 	
 	public static void method16(){
@@ -12,6 +13,16 @@ public class LoopPractice {
 		while(input<0){
 			input = Integer.parseInt(JOptionPane.showInputDialog("You did not enter a valid positive number.\n\n\n\nPlease enter a positive number"));
 		}
+		JOptionPane.showMessageDialog(null, "Thanks!");
+	}
+	
+	public static void method17(){
+		int input = 0; 
+		
+		do{
+			input = Integer.parseInt(JOptionPane.showInputDialog("Please enter an even integer"));
+		}while(input%2 != 0);
+		
 		JOptionPane.showMessageDialog(null, "Thanks!");
 	}
 }

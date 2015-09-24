3c78bed335cba149ca8287acbc9fbe5d1769ffc1
diyer <diyer@tgen.org>
2015-09-24 08:43:48 -0700

    Finished method 16

--- Loop Practice/src/LoopPractice.java
+++ Loop Practice/src/LoopPractice.java
@@ -1,4 +1,17 @@
+import javax.swing.JOptionPane;
 
 public class LoopPractice {
-
+	
+	public static void main(String[] args){
+		method16();
+	}
+	
+	public static void method16(){
+		int input = Integer.parseInt(JOptionPane.showInputDialog("Please enter a positive number"));
+		
+		while(input<0){
+			input = Integer.parseInt(JOptionPane.showInputDialog("You did not enter a valid positive number.\n\n\n\nPlease enter a positive number"));
+		}
+		JOptionPane.showMessageDialog(null, "Thanks!");
+	}
 }
